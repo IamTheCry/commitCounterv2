347119598c45277bd281d19483075c3db0b12aba
Corey <cry@guest-wireless-hsv-nat-207-151-009.usc.edu>
2018-06-20 17:11:31 -0700

    Cleanup index.js

--- client/index.js
+++ client/index.js
@@ -3,6 +3,9 @@ import { sampleData } from './sampledata.js';
 
 
 const loginButton = document.getElementById('login');
+const countButton = document.getElementById('count');
+const startDate = "2018-05-01T00:00:21Z"
+
 loginButton.addEventListener('click', () => {
   fetch('https://github.com/login/oauth/authorize', {
     mode: 'no-cors'
@@ -16,16 +19,7 @@ loginButton.addEventListener('click', () => {
   });
 });
 
-const startDate = "2018-05-01T00:00:21Z"
-
-const countButton = document.getElementById('count');
 countButton.addEventListener('click', () => {
   let result = counter(startDate, sampleData);
   document.getElementById('numberofcommits').innerHTML = result;
-  /* from sample data 
-  go through each repo
-    for each repo, go through each commit
-      if committer name matches user name
-      && is after the 'startDate' increment counter by 1
-  */
 })
