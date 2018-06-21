b2318f8ab9f7c5111145b5306e07fe7cda82096c
Corey <cry@guest-wireless-hsv-nat-207-151-009.usc.edu>
2018-06-20 17:00:11 -0700

    Implemented working counter.js logic

--- client/counter.js
+++ client/counter.js
@@ -1,4 +1,3 @@
-
 /* Grab sample data
 for each repo go through all commits
   for each commit if name = username
@@ -8,7 +7,22 @@ for each repo go through all commits
 
 const counter = (startDate, repoData) => {
   console.log(startDate);
-  console.log(repoData);
+  // console.log(repoData);
+  // assume you've already gotten the list of all users public repos
+  // sampleRepos is an array of repos
+    // each repo returned is an array of commits
+      // for each commit, check date
+      // if greater than startDate, increment counter
+  console.log(repoData.sampleArrayOfRepos);    
+  let commitCount = 0;
+  repoData.sampleArrayOfRepos.forEach(repo => {
+    repo.forEach(element => {
+      if(element.commit.author.name === 'Corey' && element.commit.author.date > startDate) {
+        commitCount++;
+      }
+    })
+  });
+  console.log(commitCount);
 };
 
 
