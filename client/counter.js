88f8dc4edd74ce4c91b7481106294d5311fa24a8
Corey <cchau005@ucr.edu>
2018-06-26 17:46:06 -0700

    Cleaned counter.js:
    removed comments and stubs

--- client/counter.js
+++ client/counter.js
@@ -1,18 +1,4 @@
-/* Grab sample data
-for each repo go through all commits
-  for each commit if name = username
-  && date greater than startDate
-  increment commit counter
-*/
-
 const counter = (startDate, repoArray, userName) => {
-  console.log(startDate);
-  // console.log(repoData);
-  // assume you've already gotten the list of all users public repos
-  // sampleRepos is an array of repos
-    // each repo returned is an array of commits
-      // for each commit, check date
-      // if greater than startDate, increment counter
   let commitCount = 0;
   repoArray.forEach(repo => {
     repo.forEach(element => {
