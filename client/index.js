219f6293185da3e21c1ede38e986bbc4f63d4fa7
Corey <cchau005@ucr.edu>
2018-06-28 17:05:53 -0700

    Removed unnecessary event listener

--- client/index.js
+++ client/index.js
@@ -1,26 +1,18 @@
 import { counter } from './counter.js';
 import { sampleArrayOfRepos } from './example.sampledata.js';
+import { token } from './authToken.js';
 
 
-const loginButton = document.getElementById('login');
 const countButton = document.getElementById('count');
 const startDate = "2018-05-01T00:00:21Z";
 const userName = "Corey";
 
-loginButton.addEventListener('click', () => {
-  fetch('https://github.com/login/oauth/authorize', {
-    mode: 'no-cors'
-  })
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log('reached the end')
-    console.log(myJson);
-  });
-});
-
 countButton.addEventListener('click', () => {
+  fetch('/repos' + token, {
+    mode: 'cors'
+  }).then((response) => console.log(response))
+  .catch(error => console.error(error))
+
   let result = counter(startDate, sampleArrayOfRepos, userName);
   document.getElementById('numberofcommits').innerHTML = result;
 })
