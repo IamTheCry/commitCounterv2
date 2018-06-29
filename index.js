1885bee80c1ad86cf842d08ea283a15fd800349a
Corey <cchau005@ucr.edu>
2018-06-28 17:04:43 -0700

    Stubbed Routes for /repos and /commits

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+
+// app.get('/', (req, res) => {
+//   res.cookie('session!', 'shouldbeencrypted');
+//   return res.sendFile(__dirname + '/client/index.html');
+// })
+
+app.get("/auth", (req, res) => {
+  let code = req.query.code;
+  console.log(code);
+  res.sendFile(__dirname + '/client/index.html');
+})
+
+app.get("/repos", (req, res) => {
+  res.send('repos')
+  // client requests repos
+})
+
+app.get("/commits", (req, res) => {
+  res.send('commits')
+  // client requests commit count
+})
+
 app.use(express.static(__dirname + '/client'))
 
-app.listen(port, () => console.log('Example app listening on port ' + port))
+app.listen(port, () => console.log('Commit Counter listening on port ' + port))
