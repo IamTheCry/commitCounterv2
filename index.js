8cf6ded499a708a364bd19deb3c584183295313e
Corey <cchau005@ucr.edu>
2018-07-03 16:05:25 -0700

    Implemented server side github API call

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const clientSecret = require('./clientSecret')
-
-
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const clientSecret = require('./clientSecret');
+const https = require('https');
 // app.get('/', (req, res) => {
 //   res.cookie('session!', 'shouldbeencrypted');
 //   return res.sendFile(__dirname + '/client/index.html');
@@ -14,6 +13,35 @@ app.get("/auth", (req, res) => {
   console.log(code);
   // send code and client secret
   console.log(clientSecret)
+  let options = {
+    hostname: 'https://github.com',
+    path: '/login/oauth/access_token?client_id=d15a7f9f6bd292b7493c&client_secret=' + clientSecret + '&code=' + code,
+    method: 'POST'
+  }
+  // const gitReq = http.get(options, (res) => {
+  //   console.log('-------------response------------', res.statusCode,res.statusMessage);
+  //   console.log(res.headers);
+  //   res.setEncoding('utf8')
+  //   res.on('data', (data) => console.log(data));
+  // });
+  // gitReq.on('error', (e) => {
+  //   console.error(`problem with request: ${e.message}`);
+  // });
+  // gitReq.end();
+  let authToken;
+  const gitReq = https.get('https://github.com/login/oauth/access_token?client_id=d15a7f9f6bd292b7493c&client_secret=' + clientSecret + '&code=' + code, (res) => {
+    
+    console.log(res.statusCode);
+    console.log(res.statusMessage);
+    res.setEncoding('utf8');
+    res.on('data', (data) => {
+      console.log(typeof(data));
+      authToken = data.split('=')[1].split('&')[0];
+    })
+  });
+  gitReq.end();
+  console.log('authtoken',authToken)
+  res.cookie('authToken',authToken)
   res.sendFile(__dirname + '/client/index.html');
 })
 
